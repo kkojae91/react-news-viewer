93b100095484461fb11f3c2f15c38749f79f446e
kkojae <jaejeung3210@gmail.com>
2020-12-16 09:41:31 +0900

    readme add

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { usePromise } from '../lib';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
@@ -17,36 +18,45 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [articles, setArticles] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        // console.log(category);
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=a0de237571414b3db6b0b6763b28c845`,
-        );
-        setArticles(response.data.articles);
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false);
-    };
-    fetchData();
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setIsLoading(true);
+  //     try {
+  //       // console.log(category);
+  //       const query = category === 'all' ? '' : `&category=${category}`;
+  //       const response = await axios.get(
+  //         `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=a0de237571414b3db6b0b6763b28c845`,
+  //       );
+  //       setArticles(response.data.articles);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //     setIsLoading(false);
+  //   };
+  //   fetchData();
+  // }, [category]);
+
+  // usePromise 커스텀 훅을 사용할땐.. 아래와 같이 사용하면 됩니다.
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=a0de237571414b3db6b0b6763b28c845`,
+    );
   }, [category]);
 
   // loading 중일땐 아래의 코드를 실행.
-  if (isLoading) {
+  if (loading) {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
   // articles 값이 유효할 경우엔 아래...
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
